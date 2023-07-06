8b78a039c0f6eb69965108c22f3cd22e34b83067
kumareshinnoraft <kumareh.baksi@innoraft.com>
2023-07-06 14:26:50 +0530

    FT2023-327: Created custom field, widgets and formatters and added permission.

---
+++ web/modules/custom/custom_rgb/src/Plugin/Field/FieldFormatter/CustomFieldDefaultFormatter.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\custom_rgb\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+
+/**
+ * Plugin implementation of the 'custom_rgb_field_default' formatter. This class
+ * is used for fetching data and showing in the page.
+ *
+ * @FieldFormatter(
+ *   id = "custom_rgb_field_default",
+ *   label = @Translation("Default"),
+ *   field_types = {"custom_rgb_field"}
+ * )
+ * 
+ * @package Drupal\custom_rgb\Plugin\Field\FieldFormatter
+ */
+class CustomFieldDefaultFormatter extends FormatterBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode)
+  {
+    $element = [];
+
+    // Getting all elements 
+    foreach ($items as $delta => $item) {
+      if ($item->six_digit_hex_code) {
+
+        $color = $item->six_digit_hex_code;
+
+        $element[$delta]['color_picker'] = [
+          '#theme' => 'custom_rgb_color_picker',
+          '#content' => '#' . $color,
+        ];
+      }
+
+      $dummy2 = $item->rgb_value;
+
+      if ($item->rgb_value) {
+
+        $values = Json::decode($item->rgb_value);
+
+        $element[$delta]['rgb_value'] = [
+          '#theme' => 'custom_rgb_color_picker',
+          '#content' => 'rgb(' . $values['r'] . ',' . $values['g'] . ',' . $values['b'] . ')',
+        ];
+      }
+
+      if ($item->color_picker) {
+        $color = $item->color_picker;
+
+        $element[$delta]['color_picker'] = [
+          '#theme' => 'custom_rgb_color_picker',
+          '#content' => $color,
+        ];
+      }
+    }
+
+    return $element;
+  }
+}
