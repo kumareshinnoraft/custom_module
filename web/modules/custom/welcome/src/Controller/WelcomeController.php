a8cb81146a4e7e8ce9d4e291643e98d4ec00070d
kumareshinnoraft <kumareh.baksi@innoraft.com>
2023-07-13 19:46:57 +0530

    FT2023-305: Changed the codes according to the drupal standards.

--- web/modules/custom/welcome/src/Controller/WelcomeController.php
+++ web/modules/custom/welcome/src/Controller/WelcomeController.php
@@ -2,9 +2,10 @@
 
 namespace Drupal\welcome\Controller;
 
-use Drupal;
-use Drupal\user\Entity\User;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\user\UserStorageInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns responses for welcome routes.
@@ -14,37 +15,61 @@ use Drupal\Core\Controller\ControllerBase;
 class WelcomeController extends ControllerBase {
 
   /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The user storage.
+   *
+   * @var \Drupal\user\UserStorageInterface
+   */
+  protected $userStorage;
+
+  /**
+   * Initilize the objects.
+   *
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current user.
+   * @param \Drupal\user\UserStorageInterface $user_storage
+   *   The user storage.
+   */
+  public function __construct(AccountProxyInterface $current_user, UserStorageInterface $user_storage) {
+    $this->currentUser = $current_user;
+    $this->userStorage = $user_storage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user'),
+      $container->get('entity_type.manager')->getStorage('user')
+    );
+  }
+
+  /**
    * Builds the response for the welcome page.
-   * 
+   *
    * @return array
    *   This array contains the title and markup message.
    */
   public function build() {
+    // Getting the current user entity.
+    $current_user = $this->userStorage->load($this->currentUser->id());
 
-    // There are three ways to get current user.
-
-
-
-    // 1st
-    // $currentUser = $this->container()->get('current_user');
-
-    //2nd, get the current user and necessary information.
-    $currentUser        = Drupal::currentUser();
-
-    // Alternative functions can be called.
-    $administerContent  = $currentUser->hasPermission('administer content');
-    $authenticated      = $currentUser->isAuthenticated();
-    $anonymous          = $currentUser->isAnonymous();
-
-    // 3rd, get complete user entity.
-    $account = User::load($currentUser->id());
-
-    // Alternative approach.
-    // $build['#title'] = $this->t('Welcome User');
- 
+    // Cache tag has been used to invalidate the cache when the user:1 tag is
+    // changed.
     return [
-      '#title'  => $this->t('Welcome ' . $account->getDisplayName()),
-      '#markup' => $this->t('This is the home page')
-    ];    
+      '#title' => $this->t('Welcome @name', ['@name' => $this->currentUser->getAccountName()]),
+      '#markup' => $this->t('This is the home page'),
+      '#cache'  => [
+        'tags' => $current_user->getCacheTags(),
+      ],
+    ];
   }
+
 }
